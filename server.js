d3eb8aea3d77a927ad5106d77a0c6fadf119a5fe
Kevin McCarthy <km.mccarthy11@gmail.com>
2019-11-04 19:29:45 -0500

    set up express for data parsing

--- server.js
+++ server.js
@@ -4,3 +4,15 @@ var bodyParser = require("body-parser");
 var app = express();
 var PORT = process.env.PORT || 3306;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type: "application/vnd.api+json"}));
+app.use(express.static(__dirname + "/app/public"));
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, function(){
+    console.log("App listening on PORT " + PORT);
+});
