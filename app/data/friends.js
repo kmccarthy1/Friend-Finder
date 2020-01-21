9d87eb10a7561890cae42f558888bb91db642ab7
Kevin McCarthy <km.mccarthy11@gmail.com>
2020-01-21 16:58:20 -0500

    added questions and friend pictures

---
+++ app/data/friends.js
@@ -0,0 +1,103 @@
+var friends = [
+    {
+      name: "Calvin",
+      photo: "http://cache2.allpostersimages.com/p/LRG/30/3012/W39BF00Z/posters/marty-feldman-comedian.jpg",
+      scores: [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+    },
+    {
+      name: "Patrick",
+      photo: "http://i.imgur.com/zlLF2kf.jpg",
+      scores: [
+        "4",
+        "2",
+        "5",
+        "1",
+        "3",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2"
+      ]
+    },
+    {
+      name: "Sweet Ken",
+      photo: "https://static.workaway.info/gfx/foto/5/5/2/8/1/552813588313/thumb/552813588313_154346649100763.jpg",
+      scores: [
+        "5",
+        "2",
+        "2",
+        "2",
+        "4",
+        "1",
+        "3",
+        "2",
+        "5",
+        "5"
+      ]
+    },
+    {
+      name: "Lewis The Nice Guy",
+      photo: "https://images.unsplash.com/photo-1530505580493-3fa88046af67?ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80",
+      scores: [
+        "3",
+        "3",
+        "4",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2",
+        "2",
+        "3"
+      ]
+    },
+    {
+      name: "William D.",
+      photo: "https://i.pinimg.com/originals/49/4f/83/494f83f0ea52388104b7d616ab0f5f11.jpg",
+      scores: [
+        "4",
+        "3",
+        "4",
+        "1",
+        "5",
+        "2",
+        "5",
+        "3",
+        "1",
+        "4"
+      ]
+    },
+    {
+      name: "BJ",
+      photo: "https://media.glamour.com/photos/56957fb55fff94d44eec32df/master/pass/entertainment-2015-03-bj-novak-mindy-project-2015-main.jpg",
+      scores: [
+        "4",
+        "4",
+        "2",
+        "3",
+        "2",
+        "2",
+        "3",
+        "2",
+        "4",
+        "5"
+      ]
+    }
+  
+  ];
+  
+  // Here we export the array. This makes it accessible to other files using require.
+  module.exports = friends;
+  
--- server.js
+++ server.js
@@ -1,19 +1,40 @@
+// ==============================================================================
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
+// ==============================================================================
+
 var express = require("express");
-var bodyParser = require("body-parser");
 
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// ==============================================================================
+
+// Tells node that we are creating an "express" server
 var app = express();
-var PORT = process.env.PORT || 3306;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.text());
-app.use(bodyParser.json({type: "application/vnd.api+json"}));
-app.use(express.static(__dirname + "/app/public"));
-//app.use(express.static("public"));
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
+
+// express.json and express.urlEncoded make it easy for our server to interpret data sent to it.
+// The code below is pretty standard.
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
-app.listen(PORT, function(){
-    console.log("App listening on PORT " + PORT);
+// ==============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// ==============================================================================
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
 });
